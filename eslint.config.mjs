a76574c0889802a322685abc93f61c730b7a7ea7
runimo <63205791+runimo@users.noreply.github.com>
2025-08-16 13:03:36 +0200

    chore: add eslint a11y plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginVueA11y from 'eslint-plugin-vuejs-accessibility'
 import prettierPlugin from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 
-export default withNuxt({
-  plugins: {
-    prettier: prettierPlugin,
+export default withNuxt(
+  ...pluginVueA11y.configs['flat/recommended'],
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
   },
-  ...prettierConfig,
-  rules: {
-    'prettier/prettier': 'error',
-  },
-})
+  prettierConfig,
+)
