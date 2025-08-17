7a17176f8fa3bb8ea80ff9743534a48c09aa4afd
runimo <63205791+runimo@users.noreply.github.com>
2025-08-17 16:41:37 +0200

    refactor: move heading above card

--- components/SignUpForm.vue
+++ components/SignUpForm.vue
@@ -1,31 +1,15 @@
 <template>
-  <nord-stack>
-    <div class="product-title">Veterinary Practice Management App</div>
-    <h1 class="main-heading">Sign up to get started</h1>
-    <form>
-      <nord-stack>
-        <nord-fieldset hide-label label="Sign Up">
-          <nord-stack>
-            <nord-input
-              expand
-              name="email"
-              label="Email Address"
-              required
-              type="email"
-            ></nord-input>
-            <nord-input
-              expand
-              name="password"
-              label="Password"
-              required
-              type="password"
-            ></nord-input>
-          </nord-stack>
-        </nord-fieldset>
-        <nord-button expand type="submit" variant="primary">Sign Up</nord-button>
-      </nord-stack>
-    </form>
-  </nord-stack>
+  <form>
+    <nord-stack>
+      <nord-fieldset hide-label label="Sign Up">
+        <nord-stack>
+          <nord-input expand name="email" label="Email Address" required type="email"></nord-input>
+          <nord-input expand name="password" label="Password" required type="password"></nord-input>
+        </nord-stack>
+      </nord-fieldset>
+      <nord-button expand type="submit" variant="primary">Sign Up</nord-button>
+    </nord-stack>
+  </form>
 </template>
 
 <script setup lang="ts">
--- layouts/signup.vue
+++ layouts/signup.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
     <nord-stack class="signup" direction="vertical" align-items="center" justify-content="center">
+      <div class="product-title">Veterinary Practice Management App</div>
+      <h1 class="main-heading">Sign up to get started</h1>
       <nord-card>
         <slot />
       </nord-card>
@@ -13,6 +15,18 @@ import NordCard from '@nordhealth/components/lib/Card'
 </script>
 
 <style scoped>
+.main-heading {
+  text-align: center;
+  font-weight: 300;
+  font-size: 1.4rem;
+}
+
+.product-title {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
 .signup {
   width: 100%;
   margin: auto;
