6b1611b63d3803cb8f97a60919b222b86c7840b7
runimo <63205791+runimo@users.noreply.github.com>
2025-08-20 23:19:27 +0200

    refactor: move success page template  to component
    - that way, concerns seem more separated - page related code is kept in the page component, but the template (apart from base styling) is kept in a separate component

---
+++ components/SignUpSuccess.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nord-stack direction="vertical" align-items="center" justify-content="center">
+      <nord-icon
+        name="interface-checked-circle"
+        size="xl"
+        color="var(--n-color-status-success)"
+      ></nord-icon>
+      <h1 class="color-success n-margin-b-xl">Success!</h1>
+      <p class="n-font-size-xl">Your account has been successfully created.</p>
+    </nord-stack>
+    <nord-stack class="n-margin-bs-xxl" direction="vertical" align-items="center">
+      <p class="n-font-size-l n-padding-is-m">
+        Log in now to start using the Veterinary Practice Management App.
+      </p>
+      <nord-button variant="primary">Login</nord-button>
+    </nord-stack>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NordButton from '@nordhealth/components/lib/Button'
+import NordIcon from '@nordhealth/components/lib/Icon'
+import NordStack from '@nordhealth/components/lib/Stack'
+</script>
+
+<style scoped>
+.color-success {
+  color: var(--n-color-status-success);
+}
+</style>
--- pages/signup/success.vue
+++ pages/signup/success.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="n-padding-b-xl success-page">
-    <nord-stack direction="vertical" align-items="center" justify-content="center">
-      <nord-icon
-        name="interface-checked-circle"
-        size="xl"
-        color="var(--n-color-status-success)"
-      ></nord-icon>
-      <h1 class="color-success n-margin-b-xl">Success!</h1>
-      <p class="n-font-size-xl">Your account has been successfully created.</p>
-    </nord-stack>
-    <nord-stack class="n-margin-bs-xxl" direction="vertical" align-items="center">
-      <p class="n-font-size-l">Log in now to start using the Veterinary Practice Management App.</p>
-      <nord-button variant="primary">Login</nord-button>
-    </nord-stack>
+    <SignUpSuccess />
   </div>
 </template>
 
 <script setup lang="ts">
-import NordButton from '@nordhealth/components/lib/Button'
-import NordIcon from '@nordhealth/components/lib/Icon'
-import NordStack from '@nordhealth/components/lib/Stack'
+import SignUpSuccess from '../../components/SignUpSuccess.vue'
 
 useHead({
   title: 'Welcome to the Veterinary Practice Management App!',
@@ -29,8 +15,5 @@ useHead({
 <style scoped>
 .success-page {
   min-height: 100vh;
-}
-.color-success {
-  color: var(--n-color-status-success);
 }
 </style>
